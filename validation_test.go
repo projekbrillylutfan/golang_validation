f012e8063cbac4556fb305040ea0fade1efad663
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-11-08 14:01:04 +0700

    mantap golang validation

--- validation_test.go
+++ validation_test.go
@@ -3,6 +3,9 @@ package belajar_golang_validation
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/go-playground/validator/v10"
@@ -221,218 +224,218 @@ func TestBasicCollection(t *testing.T) {
 	}
 }
 
-// func TestMap(t *testing.T) {
-// 	type Address struct {
-// 		City    string `validate:"required"`
-// 		Country string `validate:"required"`
-// 	}
+func TestMap(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
 
-// 	type School struct {
-// 		Name string `validate:"required"`
-// 	}
+	type School struct {
+		Name string `validate:"required"`
+	}
 
-// 	type User struct {
-// 		Id        string            `validate:"required"`
-// 		Name      string            `validate:"required"`
-// 		Addresses []Address         `validate:"required,dive"`
-// 		Hobbies   []string          `validate:"required,dive,required,min=3"`
-// 		Schools   map[string]School `validate:"dive,keys,required,min=2,endkeys,dive"`
-// 	}
+	type User struct {
+		Id        string            `validate:"required"`
+		Name      string            `validate:"required"`
+		Addresses []Address         `validate:"required,dive"`
+		Hobbies   []string          `validate:"required,dive,required,min=3"`
+		Schools   map[string]School `validate:"dive,keys,required,min=2,endkeys,dive"`
+	}
 
-// 	validate := validator.New()
-// 	request := User{
-// 		Id:   "",
-// 		Name: "",
-// 		Addresses: []Address{
-// 			{
-// 				City:    "",
-// 				Country: "",
-// 			},
-// 			{
-// 				City:    "",
-// 				Country: "",
-// 			},
-// 		},
-// 		Hobbies: []string{
-// 			"Gaming",
-// 			"Coding",
-// 			"",
-// 			"X",
-// 		},
-// 		Schools: map[string]School{
-// 			"SD": {
-// 				Name: "SD Indonesia",
-// 			},
-// 			"SMP": {
-// 				Name: "",
-// 			},
-// 			"": {
-// 				Name: "",
-// 			},
-// 		},
-// 	}
+	validate := validator.New()
+	request := User{
+		Id:   "",
+		Name: "",
+		Addresses: []Address{
+			{
+				City:    "",
+				Country: "",
+			},
+			{
+				City:    "",
+				Country: "",
+			},
+		},
+		Hobbies: []string{
+			"Gaming",
+			"Coding",
+			"",
+			"X",
+		},
+		Schools: map[string]School{
+			"SD": {
+				Name: "SD Indonesia",
+			},
+			"SMP": {
+				Name: "",
+			},
+			"": {
+				Name: "",
+			},
+		},
+	}
 
-// 	err := validate.Struct(request)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// }
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
 
-// func TestBasicMap(t *testing.T) {
-// 	type Address struct {
-// 		City    string `validate:"required"`
-// 		Country string `validate:"required"`
-// 	}
+func TestBasicMap(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
 
-// 	type School struct {
-// 		Name string `validate:"required"`
-// 	}
+	type School struct {
+		Name string `validate:"required"`
+	}
 
-// 	type User struct {
-// 		Id        string            `validate:"required"`
-// 		Name      string            `validate:"required"`
-// 		Addresses []Address         `validate:"required,dive"`
-// 		Hobbies   []string          `validate:"required,dive,required,min=3"`
-// 		Schools   map[string]School `validate:"dive,keys,required,min=2,endkeys,dive"`
-// 		Wallet    map[string]int    `validate:"dive,keys,required,endkeys,required,gt=1000"`
-// 	}
+	type User struct {
+		Id        string            `validate:"required"`
+		Name      string            `validate:"required"`
+		Addresses []Address         `validate:"required,dive"`
+		Hobbies   []string          `validate:"required,dive,required,min=3"`
+		Schools   map[string]School `validate:"dive,keys,required,min=2,endkeys,dive"`
+		Wallet    map[string]int    `validate:"dive,keys,required,endkeys,required,gt=1000"`
+	}
 
-// 	validate := validator.New()
-// 	request := User{
-// 		Id:   "",
-// 		Name: "",
-// 		Addresses: []Address{
-// 			{
-// 				City:    "",
-// 				Country: "",
-// 			},
-// 			{
-// 				City:    "",
-// 				Country: "",
-// 			},
-// 		},
-// 		Hobbies: []string{
-// 			"Gaming",
-// 			"Coding",
-// 			"",
-// 			"X",
-// 		},
-// 		Schools: map[string]School{
-// 			"SD": {
-// 				Name: "SD Indonesia",
-// 			},
-// 			"SMP": {
-// 				Name: "",
-// 			},
-// 			"": {
-// 				Name: "",
-// 			},
-// 		},
-// 		Wallet: map[string]int{
-// 			"BCA":     1000000,
-// 			"MANDIRI": 0,
-// 			"":        1001,
-// 		},
-// 	}
+	validate := validator.New()
+	request := User{
+		Id:   "",
+		Name: "",
+		Addresses: []Address{
+			{
+				City:    "",
+				Country: "",
+			},
+			{
+				City:    "",
+				Country: "",
+			},
+		},
+		Hobbies: []string{
+			"Gaming",
+			"Coding",
+			"",
+			"X",
+		},
+		Schools: map[string]School{
+			"SD": {
+				Name: "SD Indonesia",
+			},
+			"SMP": {
+				Name: "",
+			},
+			"": {
+				Name: "",
+			},
+		},
+		Wallet: map[string]int{
+			"BCA":     1000000,
+			"MANDIRI": 0,
+			"":        1001,
+		},
+	}
 
-// 	err := validate.Struct(request)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// }
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
 
-// func TestAlias(t *testing.T) {
-// 	validate := validator.New()
-// 	validate.RegisterAlias("varchar", "required,max=255")
+func TestAlias(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterAlias("varchar", "required,max=255")
 
-// 	type Seller struct {
-// 		Id     string `validate:"varchar,min=5"`
-// 		Name   string `validate:"varchar"`
-// 		Owner  string `validate:"varchar"`
-// 		Slogan string `validate:"varchar"`
-// 	}
+	type Seller struct {
+		Id     string `validate:"varchar,min=5"`
+		Name   string `validate:"varchar"`
+		Owner  string `validate:"varchar"`
+		Slogan string `validate:"varchar"`
+	}
 
-// 	seller := Seller{
-// 		Id:     "123",
-// 		Name:   "",
-// 		Owner:  "",
-// 		Slogan: "",
-// 	}
+	seller := Seller{
+		Id:     "123",
+		Name:   "",
+		Owner:  "",
+		Slogan: "",
+	}
 
-// 	err := validate.Struct(seller)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// }
+	err := validate.Struct(seller)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
 
-// func MustValidUsername(field validator.FieldLevel) bool {
-// 	value, ok := field.Field().Interface().(string)
-// 	if ok {
-// 		if value != strings.ToUpper(value) {
-// 			return false
-// 		}
-// 		if len(value) < 5 {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+func MustValidUsername(field validator.FieldLevel) bool {
+	value, ok := field.Field().Interface().(string)
+	if ok {
+		if value != strings.ToUpper(value) {
+			return false
+		}
+		if len(value) < 5 {
+			return false
+		}
+	}
+	return true
+}
 
-// func TestCustomValidationFunction(t *testing.T) {
-// 	validate := validator.New()
-// 	validate.RegisterValidation("username", MustValidUsername)
+func TestCustomValidationFunction(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterValidation("username", MustValidUsername)
 
-// 	type LoginRequest struct {
-// 		Username string `validate:"required,username"`
-// 		Password string `validate:"required"`
-// 	}
+	type LoginRequest struct {
+		Username string `validate:"required,username"`
+		Password string `validate:"required"`
+	}
 
-// 	request := LoginRequest{
-// 		Username: "EKOKK",
-// 		Password: "",
-// 	}
+	request := LoginRequest{
+		Username: "EKOKK",
+		Password: "aaaa",
+	}
 
-// 	err := validate.Struct(request)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// }
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
 
-// var regexNumber = regexp.MustCompile("^[0-9]+$")
+var regexNumber = regexp.MustCompile("^[0-9]+$")
 
-// func MustValidPin(field validator.FieldLevel) bool {
-// 	length, err := strconv.Atoi(field.Param())
-// 	if err != nil {
-// 		panic(err)
-// 	}
+func MustValidPin(field validator.FieldLevel) bool {
+	length, err := strconv.Atoi(field.Param())
+	if err != nil {
+		panic(err)
+	}
 
-// 	value := field.Field().String()
-// 	if !regexNumber.MatchString(value) {
-// 		return false
-// 	}
+	value := field.Field().String()
+	if !regexNumber.MatchString(value) {
+		return false
+	}
 
-// 	return len(value) == length
-// }
+	return len(value) == length
+}
 
-// func TestCustomValidationParameter(t *testing.T) {
-// 	validate := validator.New()
-// 	validate.RegisterValidation("pin", MustValidPin)
+func TestCustomValidationParameter(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterValidation("pin", MustValidPin)
 
-// 	type Login struct {
-// 		Phone string `validate:"required,number"`
-// 		Pin   string `validate:"required,pin=6"`
-// 	}
+	type Login struct {
+		Phone string `validate:"required,number"`
+		Pin   string `validate:"required,pin=6"`
+	}
 
-// 	request := Login{
-// 		Phone: "0904190424",
-// 		Pin:   "123123",
-// 	}
+	request := Login{
+		Phone: "0904190424",
+		Pin:   "123123",
+	}
 
-// 	err := validate.Struct(request)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 // func TestOrRule(t *testing.T) {
 // 	type Login struct {
